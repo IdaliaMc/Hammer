2ace30ce735faeb0241328a73d0b0cc95d899243
Louis <72603854+Ayatooo@users.noreply.github.com>
2022-09-14 21:21:03 +0200

    :zap: hammer done & detect enemy claim for saberFaction

--- src/main/java/fr/ayato/hammer/listeners/BreakBlockListener.java
+++ src/main/java/fr/ayato/hammer/listeners/BreakBlockListener.java
@@ -1,8 +1,13 @@
 package fr.ayato.hammer.listeners;
 
+
 import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.listeners.FactionsBlockListener;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import fr.ayato.hammer.utils.Config;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -13,72 +18,48 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+
+import java.util.*;
 
 public class BreakBlockListener implements Listener {
-    Map<UUID, BlockFace> lastBlockFace = new HashMap<>();
 
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerInteract(PlayerInteractEvent e) {
-        if (e.getAction() == Action.LEFT_CLICK_BLOCK) this.lastBlockFace.put(
-                e.getPlayer().getUniqueId(),
-                e.getBlockFace()
-        );
-    }
-
-    private void processBlock(ItemStack i, Block b, Player p) {
-        if (b.isLiquid()) return;
-        if (b.getType() == Material.BEDROCK ||
-                b.getType() == Material.BARRIER ||
-                b.getType() == Material.MOB_SPAWNER ||
-                b.getType() == Material.ENDER_CHEST ||
-                b.getType() == Material.ENDER_PORTAL ||
-                b.getType() == Material.ENDER_PORTAL_FRAME ||
-                b.getType() == Material.PORTAL
-        ) return;
-        b.breakNaturally(i);
+    public boolean isBannedBlock(Block block) {
+        List<Material> isBannedBlock = new ArrayList<>();
+        isBannedBlock.add(Material.BEDROCK);
+        isBannedBlock.add(Material.BARRIER);
+        isBannedBlock.add(Material.MOB_SPAWNER);
+        isBannedBlock.add(Material.ENDER_CHEST);
+        isBannedBlock.add(Material.ENDER_PORTAL);
+        isBannedBlock.add(Material.ENDER_PORTAL_FRAME);
+        isBannedBlock.add(Material.PORTAL);
+        return isBannedBlock.contains(block.getType());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void blockBreackEvent(BlockBreakEvent e) {
         Player player = e.getPlayer();
         ItemStack item = player.getItemInHand();
-        if (item != null && (item.getType() == Material.DIAMOND_PICKAXE || !FPlayers.getInstance().getByPlayer(player).isInEnemyTerritory()) && item.hasItemMeta() && item.getItemMeta().hasLore() && item.getItemMeta().getLore().equals(Config.getItemLore())) {
-            Player p = e.getPlayer();
-            ItemStack i = e.getPlayer().getItemInHand();
-            if (this.lastBlockFace.get(p.getUniqueId()) == null || this.lastBlockFace.get(p.getUniqueId()) == BlockFace.DOWN || this.lastBlockFace.get(p.getUniqueId()) == BlockFace.UP) {
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH_EAST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.EAST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH_EAST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH_WEST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.WEST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH_WEST), p);
-            }
-            if (this.lastBlockFace.get(p.getUniqueId()) == BlockFace.EAST || this.lastBlockFace.get(p.getUniqueId()) == BlockFace.WEST) {
-                processBlock(i, e.getBlock().getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.DOWN), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH).getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.NORTH).getRelative(BlockFace.DOWN), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH).getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.SOUTH).getRelative(BlockFace.DOWN), p);
-            }
-            if (this.lastBlockFace.get(p.getUniqueId()) == BlockFace.NORTH || this.lastBlockFace.get(p.getUniqueId()) == BlockFace.SOUTH) {
-                processBlock(i, e.getBlock().getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.DOWN), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.EAST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.WEST), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.EAST).getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.EAST).getRelative(BlockFace.DOWN), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.WEST).getRelative(BlockFace.UP), p);
-                processBlock(i, e.getBlock().getRelative(BlockFace.WEST).getRelative(BlockFace.DOWN), p
-                );
+        Player p = e.getPlayer();
+        if (item != null && item.getType() == Material.DIAMOND_PICKAXE && item.hasItemMeta() && item.getItemMeta().hasLore() && item.getItemMeta().getLore().equals(Config.getItemLore())) {
+            Faction faction = FPlayers.getInstance().getByPlayer(p).getFaction();
+            Block b = e.getBlock();
+            World w = b.getWorld();
+            for (int x = -1; x < 2; x++) {
+                for (int y = -1; y < 2; y++) {
+                    for (int z = -1; z < 2; z++) {
+                        Block block = w.getBlockAt(b.getLocation().getBlockX() + x, b.getLocation().getBlockY() + y, b.getLocation().getBlockZ() + z);
+                        try {
+                            if (!isBannedBlock(block)) {
+                                if (FactionsBlockListener.playerCanBuildDestroyBlock(p, block.getLocation(), "break", true)) {
+                                    block.breakNaturally();
+                                }
+                            } else {
+                                e.setCancelled(true);
+                            }
+                        } catch (Exception ignored) {
+                        }
+                    }
+                }
             }
         }
     }
